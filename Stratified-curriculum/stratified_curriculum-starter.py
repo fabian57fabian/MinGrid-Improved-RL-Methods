c531eba2a993e108e7f716045cab9dbeadb53b43
fabian57fabian <fabian_57@yahoo.it>
2018-10-25 16:53:23 +0200

    Removed raw frames input
    Removed all input frames because training with certain parameters stops when the mean of accurancy in a window of N log-intervals is above a certain accurancy. Last accurancy wanted is the one given to this python script (e.g. --acc=0.94)

--- Stratified-curriculum/stratified_curriculum-starter.py
+++ Stratified-curriculum/stratified_curriculum-starter.py
@@ -6,10 +6,8 @@ import argparse
 import logging
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--frames-per-train", type=int, default=100000,
-                    help="number of frames of training (default: 10e5)")
-parser.add_argument("--frames-rnd-train", type=int, default=500000,
-                    help="number of frames of training for last training with random doors (default: 5*1e5)")
+parser.add_argument("--acc", type=float, default=0.9,
+                    help="accurancy wanted for final random train (default: 0.9)")
 parser.add_argument("--env-size", default="8",
                     help="size of the model (default: 8 as 8x8)")
 args = parser.parse_args()
@@ -20,25 +18,15 @@ model="DoorKey-"+args.env_size+"x"+args.env_size+"-stratified"
 # Deltas used for training without last one (delta=1 means random over all wall locations)
 deltas = np.array([.1,.25,.4,.55,.7,.85,.92])
 
-def train(procs, delta_strat, frames, ending_acc):
-    os.system("python3 -m scripts.train --procs "+str(procs)+" --strat "+str(delta_strat)+" --algo=ppo --env " + env + " --no-instr --tb --frames="+str(frames)+" --model "+model+" --save-interval 10 --ending-acc "+str(ending_acc))
-
-def read_frames():
-    with open('storage/'+model+'/status.json') as json_data:
-        d = json.load(json_data)
-    return d['num_frames']
+def train(procs, delta_strat, ending_acc=1, N=5):
+    os.system("python3 -m scripts.train --procs "+str(procs)+" --strat "+str(delta_strat)+" --algo=ppo --env " + env + " --no-instr --tb --frames=100000000 --model "+model+" --save-interval 10 --ending-acc "+str(ending_acc))
 
 def main():
     # Starting training frames 
-    train(20,deltas[0], 10**10, 0.9)
-    frames = read_frames()
-    iter_deltas = iter(deltas)
-    next(iter_deltas)
+    train(20,deltas[0], ending_acc=0.9)
     for _delta in iter_deltas:
-        frames += args.frames_per_train
-        train(20,_delta, frames, 1)
+        train(20,_delta , ending_acc=0.9, N=20)
     # The last training with random doors
-    frames +=args.frames_rnd_train - args.frames_per_train
-    train(20,1,frames)
+    train(20,1,ending_acc=args.acc, N=20)
 
 main()
