23a6d252f7fb1fd533ae55b53680e26c88900110
fabian57fabian <fabian_57@yahoo.it>
2018-10-25 17:39:58 +0200

    Changed mean window
    Changed mean window (N variable) to better train and to be sure that trainings over intermediate deltas are strong (N=40 used)

--- Stratified-curriculum/stratified_curriculum-starter.py
+++ Stratified-curriculum/stratified_curriculum-starter.py
@@ -19,16 +19,16 @@ model="DoorKey-"+args.env_size+"x"+args.env_size+"-stratified"
 deltas = np.array([.1,.25,.4,.55,.7,.85,.92])
 
 def train(procs, delta_strat, ending_acc=1, N=5):
-    os.system("python3 -m scripts.train --procs "+str(procs)+" --strat "+str(delta_strat)+" --algo=ppo --env " + env + " --no-instr --tb --frames=100000000 --model "+model+" --save-interval 10 --ending-acc "+str(ending_acc))
+    os.system("python3 -m scripts.train --procs "+str(procs)+" --strat "+str(delta_strat)+" --algo=ppo --env " + env + " --no-instr --tb --frames=100000000 --model "+model+" --save-interval 10 --ending-acc "+str(ending_acc) + " --ending-acc-window "+str(N))
 
 def main():
     # Starting training frames 
-    train(20,deltas[0], ending_acc=0.9)
+    train(20,deltas[0], ending_acc=0.9, N=20)
     iter_deltas = iter(deltas)
     next(iter_deltas)
     for _delta in iter_deltas:
-        train(20,_delta , ending_acc=0.9, N=20)
+        train(20,_delta , ending_acc=0.9, N=40)
     # The last training with random doors
-    train(20,1,ending_acc=args.acc, N=20)
+    train(20,1,ending_acc=args.acc, N=30)
 
 main()
