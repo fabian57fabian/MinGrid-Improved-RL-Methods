5b4f3f05bdee568c27c01c6e943585f382e25db5
fabian_57 <fabian_57@yahoo.it>
2018-10-29 20:42:35 +0100

    Added multi test on sigmas
    Added a script calling multiple times stratified-curriculum-starter.py with different sigmas on 16x16 DoorKey env

--- Stratified-curriculum/stratified_curriculum-starter.py
+++ Stratified-curriculum/stratified_curriculum-starter.py
@@ -12,18 +12,19 @@ parser.add_argument("--env-size", default="8",
                     help="size of the model (default: 8 as 8x8)")
 parser.add_argument("--sigma", type=float, default=0.6,
                     help="sigma for gaussian (default: 0.6)")
+parser.add_argument("--frames", type=int, default=15000000,
+                    help="max frames to calculate (default: 15000000)")
 args = parser.parse_args()
 
 env ="MiniGrid-DoorKey-"+args.env_size+"x"+args.env_size+"-v0"
-model="DoorKey-"+args.env_size+"x"+args.env_size+"-stratified"
+model="DoorKey-"+args.env_size+"x"+args.env_size+"-stratified-sigma"+str(args.sigma)
 
 # Deltas used for training without last one (delta=1 means random over all wall locations)
 deltas = np.arange(0,1,.02)
 
-max_frames = 15000000
 
 def train(procs, delta_strat, ending_acc=1, N=5):
-    os.system("python3 -m scripts.train --procs "+str(procs)+" --strat "+str(delta_strat)+" --algo=ppo --env " + env + " --no-instr --tb --frames="+str(max_frames)+" --model "+model+" --save-interval 10 --ending-acc "+str(ending_acc) + " --ending-acc-window "+str(N))
+    os.system("python3 -m scripts.train --procs "+str(procs)+" --strat "+str(delta_strat)+" --algo=ppo --env " + env + " --no-instr --tb --frames="+str(args.frames)+" --model "+model+" --save-interval 10 --ending-acc "+str(ending_acc) + " --ending-acc-window "+str(N))
     # Save train status
     with open('storage/'+model+'/status_stratified.json', 'w') as outfile:
             json.dump({"delta:": delta_strat}, outfile)    
