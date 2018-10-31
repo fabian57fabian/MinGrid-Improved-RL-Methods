a592845b83297cd44b5e58688df2c604cb14b1a8
fabian57fabian <fabian_57@yahoo.it>
2018-10-31 14:19:20 +0100

    Changed N window and starting strat

--- Stratified-curriculum/stratified_curriculum-starter.py
+++ Stratified-curriculum/stratified_curriculum-starter.py
@@ -12,6 +12,8 @@ parser.add_argument("--env-size", default="8",
                     help="size of the model (default: 8 as 8x8)")
 parser.add_argument("--sigma", type=float, default=0.6,
                     help="sigma for gaussian (default: 0.6)")
+parser.add_argument("--strat", type=float, default=0.0,
+                    help="strating strat for stratified (default: 0.0)")
 parser.add_argument("--frames", type=int, default=15000000,
                     help="max frames to calculate (default: 15000000)")
 args = parser.parse_args()
@@ -20,7 +22,7 @@ env ="MiniGrid-DoorKey-"+args.env_size+"x"+args.env_size+"-v0"
 model="DoorKey-"+args.env_size+"x"+args.env_size+"-stratified-sigma"+str(args.sigma)
 
 # Deltas used for training without last one (delta=1 means random over all wall locations)
-deltas = np.arange(0,1,.02)
+deltas = np.arange(args.strat,1,.02)
 
 
 def train(procs, delta_strat, ending_acc=1, N=5):
@@ -30,12 +32,8 @@ def train(procs, delta_strat, ending_acc=1, N=5):
             json.dump({"delta:": delta_strat}, outfile)    
 
 def main():
-    # Starting training frames
-    train(20,deltas[0], ending_acc=0.9, N=100)
-    iter_deltas = iter(deltas)
-    next(iter_deltas)
-    for _delta in iter_deltas:
-        train(20,_delta , ending_acc=0.9, N=200)
+    for _delta in deltas:
+        train(20,_delta , ending_acc=0.9, N=100)
     # The last training with random doors
     train(20,1,ending_acc=args.acc, N=50)
 
