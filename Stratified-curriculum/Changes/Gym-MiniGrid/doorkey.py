669bfcef593f77e9d745e77b79ef6e63036a9ade
fabian57fabian <fabian_57@yahoo.it>
2018-11-21 16:27:10 +0100

    changed gaussian_int to _strat_int

--- Stratified-curriculum/Changes/Gym-MiniGrid/doorkey.py
+++ Stratified-curriculum/Changes/Gym-MiniGrid/doorkey.py
@@ -1,6 +1,7 @@
 from gym_minigrid.minigrid import *
 from gym_minigrid.register import register
 
+
 class DoorKeyEnv(MiniGridEnv):
     """
     Environment with a door and key, sparse reward
@@ -9,7 +10,7 @@ class DoorKeyEnv(MiniGridEnv):
     def __init__(self, size=8):
         super().__init__(
             grid_size=size,
-            max_steps=10*size*size
+            max_steps=10 * size * size
         )
 
     def _gen_grid(self, width, height):
@@ -25,9 +26,9 @@ class DoorKeyEnv(MiniGridEnv):
         # Create a vertical splitting wall
         # CHANGED by Fabian for stratified curriculum
         if self.delta_strat * 1.05 < 1:
-            splitIdx = self._gaussian_int(2, width-2)
+            splitIdx = self._strat_int(2, width - 2)
         else:
-            splitIdx = self._rand_int(2, width-2)
+            splitIdx = self._rand_int(2, width - 2)
         self.grid.vert_wall(splitIdx, 0)
 
         # Place the agent at a random position and orientation
@@ -35,7 +36,7 @@ class DoorKeyEnv(MiniGridEnv):
         self.start_pos = self.place_agent(size=(splitIdx, height))
 
         # Place a door in the wall
-        doorIdx = self._rand_int(1, width-2)
+        doorIdx = self._rand_int(1, width - 2)
         self.grid.set(splitIdx, doorIdx, LockedDoor('yellow'))
 
         # Place a yellow key on the left side
@@ -47,29 +48,36 @@ class DoorKeyEnv(MiniGridEnv):
 
         self.mission = "use the key to open the door and then get to the goal"
 
+
 class DoorKeyEnv5x5(DoorKeyEnv):
     def __init__(self):
         super().__init__(size=5)
+
 
 class DoorKeyEnv6x6(DoorKeyEnv):
     def __init__(self):
         super().__init__(size=6)
 
+
 class DoorKeyEnv10x10(DoorKeyEnv):
     def __init__(self):
         super().__init__(size=10)
+
 
 class DoorKeyEnv12x12(DoorKeyEnv):
     def __init__(self):
         super().__init__(size=12)
 
+
 class DoorKeyEnv14x14(DoorKeyEnv):
     def __init__(self):
         super().__init__(size=14)
 
+
 class DoorKeyEnv16x16(DoorKeyEnv):
     def __init__(self):
         super().__init__(size=16)
+
 
 register(
     id='MiniGrid-DoorKey-5x5-v0',
